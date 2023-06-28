0695edd653c034b6e62900bac849a2355f9b48e7
Anand0605 <anandvns0510@gmail.com>
2023-06-28 17:10:12 +0530

    update

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../Images/logo1.png'
+import github from '../../Images/github2.svg'
+import linkdin from '../../Images/linked2.svg'
+import instagram from '../../Images/instagram.svg'
 
 
 const Navbar = () => {
     return (
         <>
             <div className="navbar">
-                <div className="logo"></div>
-                <div className="social-media"></div>
+                <div className="logo"><img src={logo} alt="" /></div>
+                <div className="social-media">
+                    <div className="github"><img src={github} alt="" /></div>
+                    <div className="linkdin"><img src={linkdin} alt="" /></div>
+                    <div className="instagram"><img src={instagram} alt="" /></div>
+                </div>
             </div>
 
         </>
