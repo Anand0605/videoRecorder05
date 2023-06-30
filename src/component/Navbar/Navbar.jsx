e6cbc019b96cfa5f5406a97db6b68e898bd58fcd
Anand0605 <anandvns0510@gmail.com>
2023-06-30 11:37:10 +0530

    update

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import logo from '../../Images/logo6.png'
 import github from '../../Images/github2.svg'
 import linkdin from '../../Images/linked2.svg'
 import instagram from '../../Images/instagram.svg'
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -12,7 +13,7 @@ const Navbar = () => {
             <div className="navbar">
                 <div className="logo"><img src={logo} alt="" /></div>
                 <div className="social-media">
-                    <div className="github"><img src={github} alt="" /></div>
+                    <a href="https://github.com/Anand0605"><div className="github"><img src={github} alt="" /></div></a>
                     <div className="linkdin"><img src={linkdin} alt="" /></div>
                     <div className="instagram"><img src={instagram} alt="" /></div>
                 </div>
