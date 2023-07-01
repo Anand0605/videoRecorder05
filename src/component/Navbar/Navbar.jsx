1dfbe9d1a80f72b2de49f441710f5e1154d0d692
Anand0605 <anandvns0510@gmail.com>
2023-07-01 16:56:41 +0530

    update

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,7 +4,8 @@ import logo from '../../Images/logo6.png'
 import github from '../../Images/github2.svg'
 import linkdin from '../../Images/linked2.svg'
 import instagram from '../../Images/instagram.svg'
-import { NavLink } from "react-router-dom";
+import Anand from '../../Images/Anand_Gautam.jpg'
+
 
 
 const Navbar = () => {
@@ -12,9 +13,10 @@ const Navbar = () => {
         <>
             <div className="navbar">
                 <div className="logo"><img src={logo} alt="" /></div>
+                <div className="devloper-icon"><a href="https://anand-gautam.netlify.app/"><img src={Anand} alt="" /></a></div>
                 <div className="social-media">
                     <a href="https://github.com/Anand0605"><div className="github"><img src={github} alt="" /></div></a>
-                    <div className="linkdin"><img src={linkdin} alt="" /></div>
+                    <a href="https://www.linkedin.com/in/anand-kumar-gautam-733586207/"><div className="linkdin"><img src={linkdin} alt="" /></div></a>
                     <div className="instagram"><img src={instagram} alt="" /></div>
                 </div>
             </div>
